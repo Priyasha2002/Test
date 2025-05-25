c734b2ffee146b0cf4443d7af9d3d35f3ec19aed
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2025-05-25 18:03:04 +0530

    Update LoanController.cs

--- LoanController.cs
+++ LoanController.cs
@@ -59,3 +59,97 @@ namespace LoanApplicationSystem.Controllers
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanApplicationSystem.Models;
+
+namespace LoanApplicationSystem.Controllers
+{
+    public class LoanController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Apply(LoanApplication model, HttpPostedFileBase uploadDocument)
+        {
+            if (ModelState.IsValid)
+            {
+                if (uploadDocument != null && uploadDocument.ContentLength > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(uploadDocument.FileName);
+                    var path = Path.Combine(Server.MapPath("~/UploadedDocs/"), fileName);
+                    uploadDocument.SaveAs(path);
+                    model.UploadedFileName = fileName;
+                }
+
+                model.SubmissionDate = DateTime.Now;
+                db.LoanApplications.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Success");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult AdminList()
+        {
+            return View(db.LoanApplications.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult UpdateStatus(int ApplicationId, ApplicationStatus Status)
+        {
+            var application = db.LoanApplications.FirstOrDefault(a => a.Id == ApplicationId);
+            if (application != null)
+            {
+                application.Status = Status;
+                db.SaveChanges();
+                TempData["Message"] = "Status updated successfully!";
+            }
+            return RedirectToAction("AdminList");
+        }
+    }
+}
+
